99a348c39f0f6e69582f73d8ae5c7763d29fac97
Apurva Zunzunwala <zunzunwala_a@India.XoriantCorp.com>
2023-03-30 14:00:06 +0530

    Fine tune code

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,9 @@
 import "./App.css";
 import Header from "./components/Header";
-import subsectionImg from "./portfolio-img.jpeg";
+import portfolio_img from "./portfolio-img.jpeg";
+import novel_img from "./novel-final.jpg";
+
+const portfolio_grid_images=[novel_img,novel_img,novel_img,portfolio_img];
 
 function App() {
   return (
@@ -8,18 +11,13 @@ function App() {
       <Header />
       <section className="container">
         <div className="work">
-          <div className="work__subsection">
-            <img className="img__sub" src={subsectionImg} alt="work section" />
-          </div>
-          <div className="work__subsection">
-            <img className="img__sub" src={subsectionImg} alt="work section" />
-          </div>
-          <div className="work__subsection">
-            <img className="img__sub" src={subsectionImg} alt="work section" />
-          </div>
-          <div className="work__subsection">
-            <img className="img__sub" src={subsectionImg} alt="work section" />
-          </div>
+          {portfolio_grid_images.map(image => {
+            return (
+              <div className="grid_item" key={image}>
+                <img className="img__sub" src={image} alt="work section" />
+              </div>
+            )
+          })}
         </div>
       </section>
     </div>
@@ -27,3 +25,4 @@ function App() {
 }
 
 export default App;
+        
--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -1,17 +1,22 @@
 import React from "react";
 import { FaInstagram } from "react-icons/fa";
 
+const portfolio_navItems = ['Work','About'];
+
 export default function Header() {
   return (
     <div>
       <header className="App-header">
         <nav>
-          <h1>Rajnandini</h1>
-          <ul>
-            <li>Work</li>
-            <li>About</li>
-            <FaInstagram size={20} />
-          </ul>
+          <div className="header_title">
+            <h1>Rajnandini</h1>
+          </div>
+            <ul>
+              {portfolio_navItems.map(navItem => {
+                return <li key={navItem}>{navItem}</li>
+              })}
+              <FaInstagram size={20} />
+            </ul>  
         </nav>
       </header>
     </div>
